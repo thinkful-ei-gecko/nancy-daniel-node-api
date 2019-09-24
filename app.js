d2f38085a8ecaec523e3f53962e818ebc915c5ec
nphiukhao <nphiukhao@gmail.com>
2019-09-24 13:05:24 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const movies = require('./movie-data.js')
+const app = express();
+app.use(morgan('common'));
+app.use(cors());
+
+
+app.use(function validateBearerToken(req, res, next){
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    console.log('validate bearer token middleware')
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized request'})
+ 
+    }
+    next()
+})
+
+
+app.get('/movie', (req, res) => {
+
+    const genre = req.query.genre;
+    const country = req.query.country;
+    const avg_vote = req.query.avg_vote;
+
+    results = [...movies]
+    
+    if(genre){
+        results = results
+            .filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
+
+        return res.send(results)
+    }
+
+    if (country){
+        results = results
+            .filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()));
+        return res.send(results)
+    }
+
+    if (avg_vote){
+        results = results
+            .filter(movie => movie.avg_vote >= avg_vote)
+        return res.send(results)
+    }
+    res.json(results)
+
+})
+
+
+app.listen(8000, () => {
+    console.log('listening on port 8000')
+})
