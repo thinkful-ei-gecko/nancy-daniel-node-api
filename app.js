34bcc945a4b7f53ad8cdc193eac233721d1a1f1b
Daniel <herrnichols@gmail.com>
2019-09-24 16:30:09 -0400

    added helmet

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
 const cors = require('cors')
 const movies = require('./movie-data.js')
 const app = express();
+
+
 app.use(morgan('common'));
+app.use(helmet())
 app.use(cors());
 
 
