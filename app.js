2666805dd9c6b3acda9bda661d39af5f48e283e9
nphiukhao <nphiukhao@gmail.com>
2019-09-26 20:17:20 -0700

    can filter all criterias at once

--- app.js
+++ app.js
@@ -5,55 +5,46 @@ const helmet = require('helmet')
 const cors = require('cors')
 const movies = require('./movie-data.js')
 const app = express();
-
-
 app.use(morgan('common'));
-app.use(helmet())
+app.use(helmet());
 app.use(cors());
 
 
-app.use(function validateBearerToken(req, res, next){
-    const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization')
-
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(401).json({error: 'Unauthorized request'})
- 
-    }
-    next()
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
 })
 
 
-app.get('/movie', (req, res) => {
-
-    const genre = req.query.genre;
-    const country = req.query.country;
-    const avg_vote = req.query.avg_vote;
+app.get('/movie', function handleGetMovie(req, res) {
 
     let results = [...movies]
     
-    if(genre){
+    if(req.query.genre){
         results = results
-            .filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
+            .filter(movie => movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()));
 
-        return res.send(results)
+  
     }
 
-    if (country){
+    if (req.query.country){
         results = results
-            .filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()));
-        return res.send(results)
+            .filter(movie => movie.country.toLowerCase().includes(req.query.country.toLowerCase()));
+
     }
 
-    if (avg_vote){
+    if (req.query.avg_vote){
         results = results
-            .filter(movie => movie.avg_vote >= avg_vote)
-        return res.send(results)
+            .filter(movie => movie.avg_vote >= req.query.avg_vote)
+
     }
     res.json(results)
 
 })
-
 
 app.listen(8000, () => {
     console.log('listening on port 8000')
